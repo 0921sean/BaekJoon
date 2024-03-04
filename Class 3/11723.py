c2eb240a341eada8b8e999f88ac69f145a69266c
0921sean <0921sean@gmail.com>
2024-03-04 22:50:00 +0900

    [03.04]

---
+++ Class 3/11723.py
@@ -0,0 +1,34 @@
+# 시간 초과
+import sys
+input = sys.stdin.readline
+
+m = int(input().rstrip())
+S = set()
+
+for _ in range(m):
+    sen = input().rstrip()
+    cmd = sen.split()[0]
+    if cmd == 'all':
+        S = ({(i+1) for i in range(20)})
+        # S = set([(i+1) for i in range(20)])
+    elif cmd == 'empty':
+        S.clear()
+    else:
+        num = int(sen.split()[1])
+        
+        if cmd == 'add':
+            S.add(num)
+        elif cmd == 'remove':
+            S.discard(num)
+        elif cmd == 'check':
+            if num in S:
+                print(1)
+            else:
+                print(0)
+        elif cmd == 'toggle':
+            if num in S:
+                S.discard(num)
+            else:
+                S.add(num)
+    # print(S)
+                
